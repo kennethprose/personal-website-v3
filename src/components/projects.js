22d34058702d4dac4f356aeea27bb94a75545fe0
Kenneth Rose <rosekenny12@gmail.com>
2022-01-11 14:20:15 -0500

    Actual project info

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Github } from 'react-bootstrap-icons'
 import projectData from '../data/projects.json'
 
 function Projects() {
@@ -8,12 +9,15 @@ function Projects() {
             <div class="divider before:bg-slate-200 after:bg-slate-200 dark:before:bg-slate-800 dark:after:bg-slate-800"></div>
             {projectData.projects.map((e) => {
                 return (
-                    <div className='card md:card-side bg-foreground dark:bg-dark_foreground mt-10 md:drop-shadow-lg'>
+                    <div className='card md:card-side bg-foreground dark:bg-dark_foreground mt-10 md:drop-shadow-lg h-fit'>
+                        <figure className='self-center'>
+                            <img className='md:max-h-80' src={e.img} />
+                        </figure>
                         <div className='card-body'>
                             <h2 className='card-title text-primary dark:text-dark_primary'>{e.name}</h2>
                             <p className='text-secondary dark:text-dark_secondary'>{e.description}</p>
-                            <div className='card-actions'>
-                                <a className='btn' href={e.link} target="_blank" rel="noreferrer">Source</a>
+                            <div className='card-actions h-full'>
+                                <a className='btn self-end' href={e.link} target="_blank" rel="noreferrer"><Github className='text-xl' />&nbsp;&nbsp;Source</a>
                             </div>
                         </div>
                     </div>
