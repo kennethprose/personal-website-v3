46a5850e5eae239ec80f9fc061b94ad20d043ff7
Kenneth Rose <rosekenny12@gmail.com>
2023-06-25 14:27:51 -0400

    Added IntersectionObserver to add animation to project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Github } from 'react-bootstrap-icons'
 import projectData from '../data/projects.json'
 
 function Projects() {
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('loaded')
+				}
+			})
+		})
+
+		const unloadedElements = document.querySelectorAll('.unloaded')
+		unloadedElements.forEach((element) => observer.observe(element))
+
+		return () => {
+			unloadedElements.forEach((element) => observer.unobserve(element))
+		}
+	}, [])
+
 	return (
 		<div className='mx-auto min-h-full w-full px-3 mt-14'>
 			<h1 className='px-2 text-3xl text-primary dark:text-dark_primary resume_title'>
@@ -11,7 +28,7 @@ function Projects() {
 			<div class='divider before:bg-slate-200 after:bg-slate-200 dark:before:bg-slate-800 dark:after:bg-slate-800'></div>
 			{projectData.projects.map((e) => {
 				return (
-					<div className='card md:card-side bg-foreground dark:bg-dark_foreground mt-10 md:drop-shadow-lg z-10'>
+					<div className='card md:card-side bg-foreground dark:bg-dark_foreground mt-10 md:drop-shadow-lg z-10 unloaded'>
 						<figure className='self-center md:min-w-fit'>
 							<img
 								className='md:max-h-96'
